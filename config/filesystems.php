808fbe0bef37308a2d17cb601608442c41268d27
unknown <masroma75@gmail.com>
2026-03-04 12:42:51 +0700

    Fix S3 configuration: Add fallback for AWS_BUCKET and troubleshooting guide

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,10 +60,11 @@ return [
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION', 'ap-southeast-1'),
-            'bucket' => env('AWS_BUCKET'),
+            'bucket' => env('AWS_BUCKET', ''),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false, // Don't throw exceptions on failure
         ],
 
     ],
